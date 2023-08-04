f595846751db2b624ab80f653f67507634c0163f
AnnGenP <140811458+AnnGenP@users.noreply.github.com>
2023-08-04 15:52:00 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+let firstNumber = parseFloat(prompt("Введіть перше число:"));
+let secondNumber = parseFloat(prompt("Введіть друге число:"));
+let thirdNumber = parseFloat(prompt("Введіть третє число:"));
+
+let average = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(`Середнє арифметичне: ${average}`);
